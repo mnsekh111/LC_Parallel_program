134bfea71ea7c40ff708ab3a7cf3e8be7612e256
mnsekh111 <mnsekh111@gmail.com>
2016-01-24 01:47:30 -0500

    Fixed a stupid mistake in calculating avg_ett. So Stupid

--- mpi_rtt.c
+++ mpi_rtt.c
@@ -12,32 +12,34 @@ int get_uniq_tag(int task_id, int rep);
 
 int main(int argc, char * argv[]) {
 
-	int num_tasks, task_id, rc, i, j, k, l;
+	int num_tasks, task_id, rc, len, i, j, k, l;
 
 	double start_time, end_time;
 	double avg_rtt, std_dev;
 
-	char * dummy_rec_data, *dummy_send_data;
+	char * dummy_rec_data, *dummy_send_data, *processor_name = (char*) malloc(
+			100);
 	MPI_Status status;
 
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &num_tasks);
 	MPI_Comm_rank(MPI_COMM_WORLD, &task_id);
+	MPI_Get_processor_name(processor_name, &len);
 
-	double table[MSG_SIZES][num_tasks / 2][REPS];
-//	printf("%d", num_tasks);
+	double table[MSG_SIZES][REPS];
+	printf("Number of processors %d\n", num_tasks);
 	MPI_Barrier(MPI_COMM_WORLD);
 
 	int msg_size = 32;
 	int curr_pair = 0;
 
-	for (i = 0; i < MSG_SIZES; i++, msg_size *= 2) {
+	for (i = 0; i < MSG_SIZES; i++) {
 		dummy_send_data = (char *) malloc(msg_size);
 		dummy_rec_data = (char *) malloc(msg_size);
 		memset(dummy_send_data, 'A', msg_size - 1);
 
 		for (j = 0; j < num_tasks / 2; j++) {
-			if (task_id == j*2 || task_id == j*2 + 1) {
+			if (task_id == j * 2 || task_id == j * 2 + 1) {
 				avg_rtt = 0;
 				std_dev = 0;
 				for (k = 0; k < REPS; k++) {
@@ -64,8 +66,8 @@ int main(int argc, char * argv[]) {
 						}
 
 						end_time = MPI_Wtime();
-						table[i][j][k] = end_time - start_time;
-						avg_rtt += table[i][j][k];
+						table[i][k] = end_time - start_time;
+						avg_rtt += table[i][k];
 
 					} else {
 						rc = MPI_Recv(dummy_rec_data, msg_size, MPI_CHAR,
@@ -92,19 +94,23 @@ int main(int argc, char * argv[]) {
 				}
 
 				if (task_id % 2 == 0) {
+					//if (task_id == ROOT)
+					printf("\n%d ", msg_size);
+					avg_rtt/=REPS;
 					for (k = 0; k < REPS; k++) {
-						std_dev += pow(fabs(avg_rtt - table[i][j][k]), 2);
+						std_dev += pow(fabs(avg_rtt - table[i][k]), 2);
 					}
 					std_dev /= REPS;
 					std_dev = sqrt(std_dev);
-					printf("Message size %d from %d to %d --> %f %f \n", i,
-							task_id, task_id + 1, avg_rtt, std_dev);
+					printf(" %f %f", avg_rtt, std_dev);
 				}
 			}
 			MPI_Barrier(MPI_COMM_WORLD);
 		}
 		free(dummy_rec_data);
 		free(dummy_send_data);
+		msg_size *= 2;
+		//MPI_Barrier(MPI_COMM_WORLD);
 	}
 
 //for (k = 0; k < num_tasks / 2; k++) {
